e3d8fb6c5f2c894a1c035414421b4f26a9f8eba3
timzhangau <tzhang1@AU-4ND5Y52.morningstar.com>
2016-01-05 16:09:14 +1100

    create asset class

---
+++ portfolio.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 05 11:53:40 2016
+
+@author: tzhang1
+"""
+
+
+# This is create a portfolio class to feed in
+# forecasting function
+
+class Portfolio(object):
+    def __init__(self, weight, assets, correlation):
+        self.weight = weight
+        self.assets = assets
+        self.correlation = correlation # Correlation is a matrix variable her
+        
+
+        
+        
+
+
+
+
+
+
+
+
