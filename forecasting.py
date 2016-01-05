caa236d38e2dd06be9778bd18ce69fc9741de2a6
timzhangau <tzhang1@AU-4ND5Y52.morningstar.com>
2016-01-05 18:10:53 +1100

    create forecasting and portfolio classes

--- forecasting.py
+++ forecasting.py
@@ -8,20 +8,32 @@ Created on Tue Jan 05 14:12:48 2016
 # This is to create Forecasting function to calculate
 # portfolio risk and return profile
 
+import numpy as np
 from assetalloc.portfolio import Portfolio
 from assetalloc.asset import Asset
 
 class Forecasting(object):
     
     def __init__(self, portfolio, method, simulations, 
-                 initialbal, percentile, projectyear):
+                 balance, percentile, projectyear):
         self.portfolio = portfolio
         self.method = method
         self.simulations = 2000
-        self.initialbal = 100000
+        self.balance = 100000
         self.percentile = 0.05
         self.projectyear = [1, 5, 10]
-     
+        self.mean = np.array([])
+        self.stdev = np.array([])
+        self.skew = np.array([])
+        self.kurt = np.array([])
+        for asset in portfolio.assets:
+            self.mean = np.append(self.mean, asset.mean)
+            self.stdev = np.append(self.stdev, asset.stdev)
+            self.skew = np.append(self.skew, asset.skew)
+            self.kurt = np.append(self.kurt, asset.kurt)
+            
+        self.cov = portfolio.corr * np.outer(self.stdev, self.stdev)
+        
      def return_range
         
         
--- portfolio.py
+++ portfolio.py
@@ -10,10 +10,10 @@ Created on Tue Jan 05 11:53:40 2016
 # forecasting function
 
 class Portfolio(object):
-    def __init__(self, weight, assets, correlation):
+    def __init__(self, weight, assets, corr):
         self.weight = weight
         self.assets = assets
-        self.correlation = correlation # Correlation is a matrix variable her
+        self.corr = correlation # Correlation is a matrix variable her
         
 
         
