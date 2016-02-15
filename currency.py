e6ad5e5badf6186ab2477c897a19897d978da9ef
timzhangau <tzhang1@AU-4ND5Y52.morningstar.com>
2016-02-16 09:33:34 +1100

    create new currency class for exchange rate cal

---
+++ currency.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 15 16:28:23 2016
+
+@author: tzhang1
+"""
+
+# This is to create a Currency class to store currency pair attributes
+# and calculate the Fair Nominal Exchange Rate
+
