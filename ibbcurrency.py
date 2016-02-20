c075f223b6d11b6e01011e8724dcd1b389b2f944
timzhangau <tzhang1@AU-4ND5Y52.morningstar.com>
2016-02-21 00:31:27 +1100

    currency update

--- ibbcurrency.py
+++ ibbcurrency.py
@@ -17,8 +17,23 @@ if __name__ == "__main__":
     for i in range(len(currency_list)):
         currency_name = currency_list.iloc[i, 0]
         currency = Currency(currency_name)
+        # import data
         currency.import_data('C:/Users/tzhang1/FX/CMA/')
+        # calculate fair NER
         currency.cal_fair_ner()
+        # convert pandas series to pandas dataframe
+        currency_ner_fair = currency.ner_fair.to_frame()
+        currency_ner_fair.columns = [currency_name]
+        
+        # append dataframe to consolidate NER dataframe
+        if i == 0:
+            fair_ner_history = currency_ner_fair
+        else:
+            fair_ner_history = fair_ner_history.join(currency_ner_fair)
+
+        fair_ner_history.to_csv('C:/Users/tzhang1/FX/CMA/Fair_NER.csv')        
+        
+    print 'Calculation Completed'
         
         
         
