6362107c4802d5c2a4eca1683349e7647b67b6f5
timzhangau <tzhang1@morningstar.com>
2016-04-15 17:41:52 +1000

    layout methods required

--- asset.py
+++ asset.py
@@ -8,9 +8,40 @@ Created on Tue Jan 05 15:40:32 2016
 # This is to create an Asset class to store risk and 
 # return attributes
 
+import numpy as np
+import pandas as pd
+
+
+
+
 class Asset(object):
-    def __init__(self, mean, stdev, skew, kurt):
-        self.mean = mean
-        self.stdev = stdev
+    def __init__(self, g_rtn, stdev, skew, kurt):
+        self.a_rtn = a_rtn
+        self.g_rtn = g_rtn        
+        # Asset return can be modelled using different methods:
+        # Building Block, CAPM, Black-Litterman and Historical Data.
+        # This attribute is only the final output of different methods.
+        self.stdev = stdev 
+        # Risk measures can be modelled using factor-model.
+        # This attribute is only the final output of risk model.
         self.skew = skew
         self.kurt = kurt
+        self.history = history
+        self.currency = currency
+    
+    
+"""
+Methods need to be built for Asset object:
+    - Return Assumption Methods: Building Block, CAPM, Black-Litterman and Historical
+    - Risk Assumption Methods: Risk Factor Model for stdev, skew and kurt. Correlation _
+    matrix is a method under portfolio objects
+    - Conversion between arithmetic mean and geometric mean
+"""
+
+        
+        
+        
+        
+        
+        
+        
--- portfolio.py
+++ portfolio.py
@@ -14,24 +14,36 @@ class Portfolio(object):
     def __init__(self, weights, assets, corr):
         self.weights = weights # weights is a numpy array
         self.assets = assets
-        self.corr = corr
-        self.mean = np.array([])
+        self.corr = corr # Correlation is a numpy array
+        self.a_rtn_ls = np.array([])
+        self.g_rtn_ls = np.array([])
         self.stdev = np.array([])
         self.skew = np.array([])
-        self.kurt = np.array([]) # Correlation is a numpy array
+        self.kurt = np.array([])
         for asset in self.assets:
-            self.mean = np.append(self.mean, asset.mean)
-            self.stdev = np.append(self.stdev, asset.stdev)
-            self.skew = np.append(self.skew, asset.skew)
-            self.kurt = np.append(self.kurt, asset.kurt)
-        
+            self.a_rtn_ls = np.append(self.a_rtn, asset.a_rtn)
+            self.g_rtn_ls = np.append(self.g_rtn, asset.g_rtn)
+            self.stdev_ls = np.append(self.stdev, asset.stdev)
+            self.skew_ls = np.append(self.skew, asset.skew)
+            self.kurt_ls = np.append(self.kurt, asset.kurt)
+
+"""
+Methods need to be built for Portfolio object:
+    - Calculate portfolio metrics
+    - Portfolio Optimization
+    - Portfolio Forecasting
+"""
+    
+
+    
     def calculate_portfolio_stats(self):
         if np.sum(self.weights) == 1:
-            self.AR = np.dot(self.weights, self.mean)
-            self.cov = self.corr * np.outer(self.stdev, self.stdev)
+            self.a_rtn = np.dot(self.weights, self.a_rtn_ls)
+            self.g_rtn = np.dot(self.weights, self.g_rtn_ls)
+            self.cov = self.corr * np.outer(self.stdev_ls, self.stdev_ls)
             self.stdev = np.dot(np.dot(self.weights, self.cov), self.weights)
-            self.GR = (1 + self.AR) * 
-            exp(-0.5 * (self.stdev**2) * ((1+self.AR)**-2)) -1
+            self.g_rtn = (1 + self.a_rtn) * 
+            exp(-0.5 * (self.stdev**2) * ((1+self.a_rtn)**-2)) -1
         else:
             print 'Check Portfolio Weights, sum not equal to 100%'
             
